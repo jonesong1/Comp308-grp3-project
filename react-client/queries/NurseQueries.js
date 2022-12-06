6dbf6d35d4f2e61c04036ab55ec45a1e4a2259f7
dmachad7 <113929117+dmachad7@users.noreply.github.com>
2022-12-06 15:52:49 -0500

    Queries added

---
+++ react-client/queries/NurseQueries.js
@@ -0,0 +1,77 @@
+import { gql } from "@apollo/client";
+
+const QUERY_NURSE = gql`
+  query GetNurse($id: String!) {
+    nurse(id: $id) {
+      _id
+      firstName
+      lastName
+      username
+    }
+  }
+`;
+
+const QUERY_NURSES = gql`
+  query GetNurse {
+    nurses {
+      _id
+      firstName
+      lastName
+      username
+    }
+  }
+`;
+
+const ADD_NURSE = gql`
+  mutation AddCourse(
+    $firstName: String!
+    $lastName: String!
+    $username: String!
+    $password: String!
+  ) {
+    addCourse(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      password: $password
+    ) {
+      _id
+    }
+  }
+`;
+
+const UPDATE_NURSE = gql`
+  mutation UpdateNurse(
+    $id: String!
+    $firstName: String!
+    $lastName: String!
+    $username: String!
+    $password: String!
+  ) {
+    updateNurse(
+      id: $id
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      password: $password
+    ) {
+      _id
+    }
+  }
+`;
+
+const DELETE_NURSE = gql`
+  mutation DeleteNurse($id: String!) {
+    deleteNurse(id: $id) {
+      _id
+    }
+  }
+`;
+
+export {
+  QUERY_NURSE,
+  QUERY_NURSES,
+  ADD_NURSE,
+  UPDATE_NURSE,
+  DELETE_NURSE,
+};
